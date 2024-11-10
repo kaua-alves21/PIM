82c23689525a4fe270983010248933c1c92d1d79
SamuelOB <samuel_oliveirab@hotmail.com>
2024-11-09 22:02:42 -0300

    add meta tag

--- html/login.php
+++ html/login.php
@@ -18,6 +18,7 @@ if (isset($_SESSION["logado"])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login</title>
     <link rel="stylesheet" href="../css/styles.css">
 </head>

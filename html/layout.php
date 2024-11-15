062166f47502c000178ebaad9a3682af0090afe1
Kaua Alves <105435529+kaua-alves21@users.noreply.github.com>
2024-11-14 22:34:07 -0300

    Update layout.php

--- html/layout.php
+++ html/layout.php
@@ -14,7 +14,7 @@
 
     <!-- Cabeçalho -->
     <header>
-        <h1>Startup Segurança Alimentar</h1>
+        <h1>CIFU - Controle Inteligente de Fazendo Urbana</h1>
         <p>&nbsp;</p>
     </header>
 
@@ -62,7 +62,7 @@
 
     <!-- Rodapé -->
     <footer>
-        <p>&copy; 2024 Startup de Segurança Alimentar. Todos os direitos reservados.</p>
+        <p>&copy; 2024 CIFU - Controle Inteligente de Fazendo Urbana. Todos os direitos reservados.</p>
     </footer>
 
     <script src="../js/load-content.js"></script>

1c221699967e1ec8eeaaedc10e73be71aa3b551c
Kaua Alves <105435529+kaua-alves21@users.noreply.github.com>
2024-11-15 22:47:46 -0300

    Update cadastro.php

--- html/cadastro.php
+++ html/cadastro.php
@@ -1,4 +1,3 @@
-<!-- cadastro.php -->
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -8,12 +7,13 @@
     <title>Cadastro</title>
     <link rel="stylesheet" href="../css/styles.css">
     <link rel="stylesheet" href="../css/cadastro.css">
+    <script src="../js/validacao.js" defer></script> <!-- Script externo -->
 </head>
 <body>
 
     <!-- Cabeçalho -->
     <header>
-        <h1>CIFU - Controle Inteligente de Fazendo Urbana</h1>
+        <h1>CIFU - Controle Inteligente de Fazenda Urbana</h1>
         <p>&nbsp;</p>
     </header>
 
@@ -35,7 +35,7 @@
         <h2>Cadastro de Novo Usuário</h2>
         <div class="login-container">
             <div class="login-form">
-                <form action="processa_cadastro.php" method="post">
+                <form id="form-cadastro" action="processa_cadastro.php" method="post">
                     <label for="nome">Nome Completo:</label>
                     <input type="text" name="nome" id="nome" placeholder="Nome Completo" required>
 
@@ -60,6 +60,8 @@
 
     <!-- Rodapé -->
     <footer>
-        <p>&copy; 2024 CIFU - Controle Inteligente de Fazendo Urbana. Todos os direitos reservados.</p>
+        <p>&copy; 2024 CIFU - Controle Inteligente de Fazenda Urbana. Todos os direitos reservados.</p>
     </footer>
+
+</body>
 </html>
